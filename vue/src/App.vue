ec78ef9f3f99a9f2cd371e4e6ffc73d952ae0add
Fady Elabed <fady.elabed@student.ehb.be>
2021-04-25 18:55:02 +0200

    landing page and setup project

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
+        <div class="loader">
+          <div class="wrapping-content">
+
+      <img src="@/assets/loadingscreen.gif" alt="">
+                </div>
+
+    </div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <transition name="fade" mode="out-in">
     <router-view/>
+    </transition>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import 'assets/style.css';
+@import url('https://fonts.googleapis.com/css2?family=Sora:wght@400;700;800&display=swap');
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+@font-face {
+  font-family: "Miratrix";
+  src: local("Miratrix"),
+   url(./fonts/Miratrix-Normal.otf) format("opentype");
 }
 </style>
