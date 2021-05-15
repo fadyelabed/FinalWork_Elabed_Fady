93ad70be98bcfa41714fe28d47102e4fd094c9f6
Fady Elabed <fady.elabed@student.ehb.be>
2021-05-15 18:47:40 +0200

    home layout done

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
 
-  <div class="home">
-          <router-link to="/nav">
-      <img class="logo-top" src="@/assets/logo1_blue.svg" alt="">
+  <div class="home intro">
+          <router-link to="/splash">
+      <img data-cursor-hover @click="play" class="logo-top" src="@/assets/logo1_blue.svg" alt="">
           </router-link>
       <article>
     <kinesis-container>
@@ -23,23 +23,51 @@
     </kinesis-element>
     </kinesis-container>
       </article>
-      <section>
+      <section class="fewfacts" >
         <div class="wrapping-content">
           <article>
-              <h2>a few facts</h2>
+            <kinesis-container>
+                <kinesis-element :strength="12"  type="depth" >
+                <h2>a few facts</h2>
+                </kinesis-element>
+            </kinesis-container>
               <p>it was reported that Spotify streams of the world’s biggest hits were actually down by..</p>
           </article>
           <article class="numberfact">
               <h2>11%</h2>
           </article>
         </div>
-
           <figure>
           </figure>
-            <figure>
+            <figure class="right-figure">
           </figure>
       </section>
-
+      <section class="facts">
+          <article>
+            <p>Pollstar estimated the total lost revenue for the live music industry in 2020 at more than..</p>
+                        <h2>30$ billion</h2>
+          </article>
+      </section>
+    <section class="outro-home">
+        <div class="wrapping-content">
+            <h3>Which also lead to a wave of cancellations of events, festivals, and tours</h3>
+            <h1>And thus began the rise of virtual events</h1>
+            <article>
+            <p>2020, the year that drastically changed a lot of industries, one of them certainly being the music industry.
+            </p>
+            <p>An occurrence that some seemed to take a bit too lightly. Knowing that live performances account for 50% of the global music industry’s total revenue, things needed to change, quickly. Since then we’ve seen plenty of really innovative and creative ways of how companies adapted to the circumstances, few of which you’ll see mentioned. But before we get to those, I’ll talk more about the specifics.</p>
+            </article>
+            <!-- <h4>welcome to pandeaming</h4> -->
+        </div>
+    </section>
+    <section class="upnext">
+    <kinesis-container>
+      <kinesis-element :strength="18"  type="translate" >
+      <h1>up next</h1>
+      </kinesis-element>
+    </kinesis-container>
+      <h2>sales and streaming</h2>
+    </section>
     <cursor-fx>
     </cursor-fx>
   </div>
@@ -56,11 +84,19 @@ export default {
   },
   methods: {
     startAnimations: function () {
-      gsap.from('article h1', {
+      gsap.from('.intro h1', {
         duration: 1.5,
         opacity: 0,
         delay: 3.4,
         y: 55,
+        stagger: 0.2,
+        ease: 'power3.out'
+      })
+      gsap.from('figure', {
+        duration: 1.8,
+        opacity: 0,
+        delay: 8,
+        y: 85,
         stagger: 0.2,
         ease: 'power3.out'
       })
